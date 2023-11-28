79080655f4cd3a05513ade0eeb6e98438fecb6e4
Tempted <2374704101@qq.com>
2023-11-28 12:45:39 +0800

    树形组件的应用

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -1,17 +1,39 @@
 <template>
   <div class="container">
     <div class="app-container">
-      组织架构
+      <el-tree :data="depts" default-expand-all :props="defaultProps"/>
     </div>
   </div>
 </template>
 
 <script>
  export default {
-   name: 'Department'
+   name: 'Department',
+   data() {
+     return {
+       // 数据属性
+       depts: [{
+         name: '传智教育',
+         children: [{
+           name: '总裁办'
+         }, {
+           name: '行政部'
+         }, {
+           name: '人事部'
+         }]
+       }],
+       defaultProps: {
+         label: 'name',
+         children: 'children'
+       }
+     }
+   }
  }
 </script>
 
 <style scoped lang="scss">
-
+.app-container {
+  padding: 30px 140px;
+  font-size: 14px;
+}
 </style>
